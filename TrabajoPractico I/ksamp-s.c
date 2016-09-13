0334c66d01bb8b9dbcc16b21c32ee1af5e5b9e30
martintarres <martintarres@gmail.com>
2016-09-12 21:06:15 -0300

    clase numeroProcesos

--- TrabajoPractico I/ksamp-s.c
+++ TrabajoPractico I/ksamp-s.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+void numeroProcesos(char* procesos){
+	FILE *archivo;
+			char caracter[100];
+			archivo = fopen("/proc/stat","r");
+			char *p=NULL;
+
+			if (archivo == NULL){
+
+					printf("\nError de apertura del archivo. \n\n");
+			        }else{
+
+
+				    while (feof(archivo) == 0)
+				    {
+					 fgets(caracter, 100, archivo);
+
+					p=strstr (caracter , "processes");
+
+					if(p!=NULL)
+							break;
+
+				    }
+				    strcpy(procesos,p);
+
+	}
+
+}
 void cambioContexto(char* cambio){
 	FILE *archivo;
 		char caracter[100];
@@ -11,8 +38,6 @@ void cambioContexto(char* cambio){
 				printf("\nError de apertura del archivo. \n\n");
 		        }else{
 
-
-			    printf("\nEl contenido del archivo es:\n");
 
 			    while (feof(archivo) == 0)
 			    {
@@ -32,7 +57,10 @@ void cambioContexto(char* cambio){
 
 int main(int argc, char **argv){
 	char cambio[30];
+	char procesos[30];
 	cambioContexto(cambio);
-	printf("%s",cambio);
+	printf("Numero de cambios de contexto: %s",cambio);
+	numeroProcesos(procesos);
+	printf("Numero de procesos: %s",procesos);
 	return 0;
 }
