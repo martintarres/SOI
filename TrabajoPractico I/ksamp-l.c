b963bbbaa7cba7d34f2e45e61f6f33d37219117e
martintarres <martintarres@gmail.com>
2016-09-13 20:35:01 -0300

    clase memoriaDisponible y memoriaLibre

--- TrabajoPractico I/ksamp-l.c
+++ TrabajoPractico I/ksamp-l.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
 
+void memoriaTotal(char* memTotal){
+				FILE *archivo;
+				char caracter[100];
+				archivo = fopen("/proc/meminfo","r");
+				char *p=NULL;
+
+				if (archivo == NULL){
+
+						printf("\nError de apertura del archivo. \n\n");
+				        }else{
+
+
+					    while (feof(archivo) == 0)
+					    {
+						 fgets(caracter, 100, archivo);
+
+						p=strstr (caracter , "MemTotal:");
+
+						if(p!=NULL)
+								break;
+
+					    }
+					    strcpy(memTotal,p);
+
+		}
+
+}
+void memoriaDisponible(char* memDispo){
+				FILE *archivo;
+				char caracter[100];
+				archivo = fopen("/proc/meminfo","r");
+				char *p=NULL;
+
+				if (archivo == NULL){
+
+						printf("\nError de apertura del archivo. \n\n");
+				        }else{
+
+
+					    while (feof(archivo) == 0)
+					    {
+						 fgets(caracter, 100, archivo);
+
+						p=strstr (caracter , "MemAvailable:");
+
+						if(p!=NULL)
+								break;
+
+					    }
+					    strcpy(memDispo,p);
+
+		}
+
+}
 void peticionesDisco(){
 	FILE *archivo;
 				char caracter[100];
@@ -28,9 +82,9 @@ void peticionesDisco(){
 					   sscanf(p,"sda %u", &lecturas);
 					   sscanf(p,"sda %*u %*u %*u %*u %u", &escrituras);
 					   total=lecturas + escrituras;
-					   printf("%u\n", lecturas);
-					   printf("%u\n", escrituras);
-					   printf("Total de lecturas pedidas a disco:%u", total);
+					 //  printf("%u\n", lecturas);
+					 //  printf("%u\n", escrituras);
+					   printf("Total de lecturas pedidas a disco :%u", total);
 					   printf("\n");
 		}
 
@@ -38,7 +92,11 @@ void peticionesDisco(){
 
 
 int main(int argc, char **argv){
-	//char pedidosDisco[100];
+	char memTotal[50];
+	char memDispo[100];
 	peticionesDisco();
-//	printf("Numero de pedidos a disco: %s",pedidosDisco);
+	memoriaTotal(memTotal);
+	printf("%s",memTotal);
+	memoriaDisponible(memDispo);
+	printf("%s",memDispo);
 }
